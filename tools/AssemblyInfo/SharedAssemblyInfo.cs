1f1c2b5958c837b9f0dd97ee18ea40d05bd2c014
dmshbld <dmsh@microsoft.com>
2016-12-02 14:51:22 +0800

    dmshbld increased build number to 1

--- tools/AssemblyInfo/SharedAssemblyInfo.cs
+++ tools/AssemblyInfo/SharedAssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.4.1.0")]
-[assembly: AssemblyFileVersion("0.4.1.31")]
+[assembly: AssemblyFileVersion("0.4.1.1")]
 
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Microsoft Azure Storage")]
