e67626402e6adff3ecfb462333fd371665e9c63c
Emma Zhu <emmazhu@microsoft.com>
2017-04-12 14:35:45 +0800

    Upgrade DMLib version to 0.5.1

--- tools/AssemblyInfo/SharedAssemblyInfo.cs
+++ tools/AssemblyInfo/SharedAssemblyInfo.cs
@@ -12,8 +12,8 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.145")]
+[assembly: AssemblyVersion("0.5.1.0")]
+[assembly: AssemblyFileVersion("0.5.1.0")]
 
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Microsoft Azure Storage")]
