994b7b084c5b9962a55bbe28029a2339e47cab99
Emma Zhu <emmazhu@microsoft.com>
2017-05-19 11:18:43 +0800

    Enable including DocXML in Nuget package.

--- tools/nupkg/buildNupkg.cmd
+++ tools/nupkg/buildNupkg.cmd
@@ -6,8 +6,10 @@ pushd ..\..
 del /q /f *.nupkg
 copy .\lib\bin\Release\Microsoft.WindowsAzure.Storage.DataMovement.dll .\tools\nupkg\lib\net45
 copy .\lib\bin\Release\Microsoft.WindowsAzure.Storage.DataMovement.pdb .\tools\nupkg\lib\net45
+copy .\lib\bin\Release\Microsoft.WindowsAzure.Storage.DataMovement.XML .\tools\nupkg\lib\net45
 copy .\netcore\Microsoft.WindowsAzure.Storage.DataMovement\bin\Release\netstandard1.6\Microsoft.WindowsAzure.Storage.DataMovement.dll .\tools\nupkg\lib\netstandard1.6
 copy .\netcore\Microsoft.WindowsAzure.Storage.DataMovement\bin\Release\netstandard1.6\Microsoft.WindowsAzure.Storage.DataMovement.pdb .\tools\nupkg\lib\netstandard1.6
+copy .\netcore\Microsoft.WindowsAzure.Storage.DataMovement\bin\Release\netstandard1.6\Microsoft.WindowsAzure.Storage.DataMovement.xml .\tools\nupkg\lib\netstandard1.6
 .\.nuget\nuget.exe pack .\tools\nupkg\Microsoft.Azure.Storage.DataMovement.nuspec
 popd
 rmdir /s /q .\lib
