fdef901d58a21f803cc6e261ceef68849e2f58c2
Emma Zhu <emmazhu@microsoft.com>
2017-09-04 10:23:48 +0800

    Upgrade to .Net Core 2.0

--- tools/nupkg/buildNupkg.cmd
+++ tools/nupkg/buildNupkg.cmd
@@ -1,15 +1,15 @@
 pushd %~dp0
 rmdir /s /q .\lib
 mkdir .\lib\net45
-mkdir .\lib\netstandard1.6
+mkdir .\lib\netstandard2.0
 pushd ..\..
 del /q /f *.nupkg
 copy .\lib\bin\Release\Microsoft.WindowsAzure.Storage.DataMovement.dll .\tools\nupkg\lib\net45
 copy .\lib\bin\Release\Microsoft.WindowsAzure.Storage.DataMovement.pdb .\tools\nupkg\lib\net45
 copy .\lib\bin\Release\Microsoft.WindowsAzure.Storage.DataMovement.XML .\tools\nupkg\lib\net45
-copy .\netcore\Microsoft.WindowsAzure.Storage.DataMovement\bin\Release\netstandard1.6\Microsoft.WindowsAzure.Storage.DataMovement.dll .\tools\nupkg\lib\netstandard1.6
-copy .\netcore\Microsoft.WindowsAzure.Storage.DataMovement\bin\Release\netstandard1.6\Microsoft.WindowsAzure.Storage.DataMovement.pdb .\tools\nupkg\lib\netstandard1.6
-copy .\netcore\Microsoft.WindowsAzure.Storage.DataMovement\bin\Release\netstandard1.6\Microsoft.WindowsAzure.Storage.DataMovement.xml .\tools\nupkg\lib\netstandard1.6
+copy .\netcore\Microsoft.WindowsAzure.Storage.DataMovement\bin\Release\netstandard2.0\Microsoft.WindowsAzure.Storage.DataMovement.dll .\tools\nupkg\lib\netstandard2.0
+copy .\netcore\Microsoft.WindowsAzure.Storage.DataMovement\bin\Release\netstandard2.0\Microsoft.WindowsAzure.Storage.DataMovement.pdb .\tools\nupkg\lib\netstandard2.0
+copy .\netcore\Microsoft.WindowsAzure.Storage.DataMovement\bin\Release\netstandard2.0\Microsoft.WindowsAzure.Storage.DataMovement.xml .\tools\nupkg\lib\netstandard2.0
 .\.nuget\nuget.exe pack .\tools\nupkg\Microsoft.Azure.Storage.DataMovement.nuspec
 popd
 rmdir /s /q .\lib
