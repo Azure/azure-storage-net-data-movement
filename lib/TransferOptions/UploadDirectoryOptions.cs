a54edcd148dd750cff0ca6aac3f8d369eea5067e
Emma Zhu <emmazhu@microsoft.com>
2017-11-02 15:32:27 +0800

    Implement follow-symlink

--- lib/TransferOptions/UploadDirectoryOptions.cs
+++ lib/TransferOptions/UploadDirectoryOptions.cs
@@ -6,16 +6,40 @@
 namespace Microsoft.WindowsAzure.Storage.DataMovement
 {
     using Microsoft.WindowsAzure.Storage.Blob;
+    using Microsoft.WindowsAzure.Storage.DataMovement.Interop;
+    using System;
 
     /// <summary>
     /// Represents a set of options that may be specified for upload directory operation
     /// </summary>
     public sealed class UploadDirectoryOptions : DirectoryOptions
     {
+        private bool followSymlink = false;
         /// <summary>
         /// Gets or sets type of destination blob. This option takes effect only when uploading to Azure blob storage.
         /// If blob type is not specified, BlockBlob is used.
         /// </summary>
         public BlobType BlobType { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether to follow symlinked directories. This option only works in Unix/Linux platforms.
+        /// </summary>
+        public bool FollowSymlink
+        {
+            get
+            {
+                return this.followSymlink;
+            }
+
+            set
+            {
+                if (value && !CrossPlatformHelpers.IsLinux)
+                {
+                    throw new PlatformNotSupportedException();
+                }
+
+                this.followSymlink = value;
+            }
+        }
     }
 }
