8a77c12034ef0f01c54d3dd2c27202a5bfc3fac6
Emma Zhu <emmazhu@microsoft.com>
2018-02-10 12:10:30 +0800

    Exclude nupkg script from DMLib's nuget package.

--- tools/nupkg/buildNupkg.cmd
+++ tools/nupkg/buildNupkg.cmd
@@ -1,16 +1,17 @@
 pushd %~dp0
-rmdir /s /q .\lib
-mkdir .\lib\net45
-mkdir .\lib\netstandard2.0
+rmdir /s /q .\package
+mkdir .\package\lib\net45
+mkdir .\package\lib\netstandard2.0
 pushd ..\..
 del /q /f *.nupkg
-copy .\lib\bin\Release\Microsoft.WindowsAzure.Storage.DataMovement.dll .\tools\nupkg\lib\net45
-copy .\lib\bin\Release\Microsoft.WindowsAzure.Storage.DataMovement.pdb .\tools\nupkg\lib\net45
-copy .\lib\bin\Release\Microsoft.WindowsAzure.Storage.DataMovement.XML .\tools\nupkg\lib\net45
-copy .\netcore\Microsoft.WindowsAzure.Storage.DataMovement\bin\Release\netstandard2.0\Microsoft.WindowsAzure.Storage.DataMovement.dll .\tools\nupkg\lib\netstandard2.0
-copy .\netcore\Microsoft.WindowsAzure.Storage.DataMovement\bin\Release\netstandard2.0\Microsoft.WindowsAzure.Storage.DataMovement.pdb .\tools\nupkg\lib\netstandard2.0
-copy .\netcore\Microsoft.WindowsAzure.Storage.DataMovement\bin\Release\netstandard2.0\Microsoft.WindowsAzure.Storage.DataMovement.xml .\tools\nupkg\lib\netstandard2.0
-.\.nuget\nuget.exe pack .\tools\nupkg\Microsoft.Azure.Storage.DataMovement.nuspec
+copy .\lib\bin\Release\Microsoft.WindowsAzure.Storage.DataMovement.dll .\tools\nupkg\package\lib\net45
+copy .\lib\bin\Release\Microsoft.WindowsAzure.Storage.DataMovement.pdb .\tools\nupkg\package\lib\net45
+copy .\lib\bin\Release\Microsoft.WindowsAzure.Storage.DataMovement.XML .\tools\nupkg\package\lib\net45
+copy .\netcore\Microsoft.WindowsAzure.Storage.DataMovement\bin\Release\netstandard2.0\Microsoft.WindowsAzure.Storage.DataMovement.dll .\tools\nupkg\package\lib\netstandard2.0
+copy .\netcore\Microsoft.WindowsAzure.Storage.DataMovement\bin\Release\netstandard2.0\Microsoft.WindowsAzure.Storage.DataMovement.pdb .\tools\nupkg\package\lib\netstandard2.0
+copy .\netcore\Microsoft.WindowsAzure.Storage.DataMovement\bin\Release\netstandard2.0\Microsoft.WindowsAzure.Storage.DataMovement.xml .\tools\nupkg\package\lib\netstandard2.0
+copy .\tools\nupkg\Microsoft.Azure.Storage.DataMovement.nuspec .\tools\nupkg\package
+.\.nuget\nuget.exe pack .\tools\nupkg\package\Microsoft.Azure.Storage.DataMovement.nuspec
 popd
-rmdir /s /q .\lib
+rmdir /s /q .\package
 popd
