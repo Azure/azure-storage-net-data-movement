24f4ad083cb1de70d5029f105d87f28e66db16ce
Victor Nate Graf <vigraf@microsoft.com>
2018-04-02 11:18:06 -0700

    Rename offset to satisfy CodeAnalysis

--- lib/PipelineMemoryStream.cs
+++ lib/PipelineMemoryStream.cs
@@ -12,17 +12,26 @@ namespace Microsoft.WindowsAzure.Storage.DataMovement
     using System.Collections.Generic;
     using System.Threading;
 
-    // The following class is designed under the assumption that writes much are smaller on average than the buffer-size
-    // It is still correct without this assumption, but it's performance could be improved
-    // This is almost certainly a fair assumption for buffer-sizes >= 4 MB (the default)
-
+    /// <summary>
+    /// A memory stream class designed to act as a data pipeline
+    /// Data written to this stream fills a segmented buffer. As each segment is filled it is passed to data receiver via
+    /// a callback. This callback effectively transfers owenership of the buffer to the receiver, allowing it to be used
+    /// and returned to the buffer pool.
+    /// </summary>
+    /// <remarks>
+    /// The following class is designed under the assumption that writes much are smaller on average than the buffer-size
+    /// It is still correct without this assumption, but it's performance could be improved
+    /// This is almost certainly a fair assumption for buffer-sizes >= 4 MB (the default)
+    ///
+    /// Any unused buffer segments will be released to the <c>MemoryManager</c> provided on construction when <c>Dispose</c> is called
+    /// </remarks>
     internal class PipelineMemoryStream : Stream
     {
         private byte[][] buffers = null;
-        private int index = 0;    // of the current buffer
-        private int length = 0;   // including data which has been returned
-        private int offset = 0;   // within current private buffer
-        private int position = 0; // across all buffers
+        private int index = 0;       // index of the current buffer
+        private int length = 0;      // total amount of buffer space in this stream
+        private int writeOffset = 0; // offset within current private buffer
+        private int position = 0;    // position as an offset from the start of all buffer space
 
         private MemoryManager manager; // Used to return unused buffers
         private Action<byte[], int, int> callback;
@@ -41,15 +50,15 @@ namespace Microsoft.WindowsAzure.Storage.DataMovement
 
         public override void Flush()
         {
-            if (this.offset > 0) {
+            if (this.writeOffset > 0) {
                 // After this call, the receiver owns the sent buffer
-                this.callback(this.buffers[this.index], 0, this.offset);
+                this.callback(this.buffers[this.index], 0, this.writeOffset);
 
                 // Move position forward by the remaining buffer length
                 // Any unsued space in the sent buffer is lost to us
-                this.position += this.buffers[this.index].Length - this.offset;
+                this.position += this.buffers[this.index].Length - this.writeOffset;
                 this.buffers[this.index] = null;
-                this.offset = 0;
+                this.writeOffset = 0;
                 this.index += 1;
             }
         }
@@ -77,7 +86,7 @@ namespace Microsoft.WindowsAzure.Storage.DataMovement
 
             while (remaining > 0)
             {
-                int availible = this.buffers[this.index].Length - this.offset;
+                int availible = this.buffers[this.index].Length - this.writeOffset;
                 if (availible <= 0) {
                     this.Flush();
                     availible = this.buffers[this.index].Length;
@@ -87,9 +96,9 @@ namespace Microsoft.WindowsAzure.Storage.DataMovement
                 int n = remaining > availible ? availible : remaining;
 
                 //Write n bytes to the current chunk
-                Array.Copy(buffer, offset, this.buffers[this.index], this.offset, n);
+                Array.Copy(buffer, offset, this.buffers[this.index], this.writeOffset, n);
                 offset += n;
-                this.offset += n;
+                this.writeOffset += n;
                 this.position += n;
                 remaining -= n;
             }
