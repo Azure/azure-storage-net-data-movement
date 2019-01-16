60ea2f2dbe10533562f069dc9589389266a8ba7a
Emma Zhu <emmazhu@microsoft.com>
2019-01-16 10:22:25 +0800

    Update DMLib version to 0.9.1

--- tools/nupkg/buildDebugNupkg.cmd
+++ tools/nupkg/buildDebugNupkg.cmd
@@ -2,13 +2,13 @@ pushd %~dp0
 rmdir /s /q .\workspace
 mkdir .\workspace
 copy .\Microsoft.Azure.Storage.DataMovement.nuspec .\workspace
-mkdir .\workspace\lib\net45
+mkdir .\workspace\lib\net452
 mkdir .\workspace\lib\netstandard2.0
 pushd ..\..
 del /q /f *.nupkg
-copy .\lib\bin\Debug\Microsoft.WindowsAzure.Storage.DataMovement.dll .\tools\nupkg\workspace\lib\net45
-copy .\lib\bin\Debug\Microsoft.WindowsAzure.Storage.DataMovement.pdb .\tools\nupkg\workspace\lib\net45
-copy .\lib\bin\Debug\Microsoft.WindowsAzure.Storage.DataMovement.XML .\tools\nupkg\workspace\lib\net45
+copy .\lib\bin\Debug\Microsoft.WindowsAzure.Storage.DataMovement.dll .\tools\nupkg\workspace\lib\net452
+copy .\lib\bin\Debug\Microsoft.WindowsAzure.Storage.DataMovement.pdb .\tools\nupkg\workspace\lib\net452
+copy .\lib\bin\Debug\Microsoft.WindowsAzure.Storage.DataMovement.XML .\tools\nupkg\workspace\lib\net452
 copy .\netcore\Microsoft.WindowsAzure.Storage.DataMovement\bin\Debug\netstandard2.0\Microsoft.WindowsAzure.Storage.DataMovement.dll .\tools\nupkg\workspace\lib\netstandard2.0
 copy .\netcore\Microsoft.WindowsAzure.Storage.DataMovement\bin\Debug\netstandard2.0\Microsoft.WindowsAzure.Storage.DataMovement.pdb .\tools\nupkg\workspace\lib\netstandard2.0
 copy .\netcore\Microsoft.WindowsAzure.Storage.DataMovement\bin\Debug\netstandard2.0\Microsoft.WindowsAzure.Storage.DataMovement.xml .\tools\nupkg\workspace\lib\netstandard2.0
