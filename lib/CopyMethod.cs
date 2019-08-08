0d8b0de71669ecd7078e262c6e308d64b51cf268
Emma Zhu <emmazhu@microsoft.com>
2019-08-08 08:54:21 +0800

    Update comments for CopyMethod.

--- lib/CopyMethod.cs
+++ lib/CopyMethod.cs
@@ -13,18 +13,21 @@ namespace Microsoft.Azure.Storage.DataMovement
     public enum CopyMethod
     {
         /// <summary>
-        /// To read data from source to memory and then write the data in memory to destination.
+        /// To download data from source to memory, and upload the data from memory to destination.
         /// </summary>
         SyncCopy,
 
         /// <summary>
         /// To send a start copy request to azure storage to let it do the copying,
-        /// and monitor the copying progress until the copy finished.
+        /// and monitor the copying progress until the copy completed.
         /// </summary>
         ServiceSideAsyncCopy,
 
         /// <summary>
-        /// To copy content of each chunk with with "Put Block From URL", "Append Block From URL" or "Put Page From URL".
+        /// To copy content of each chunk with with Put Block From URL, Append Block From URL or Put Page From URL.
+        /// See <c>https://docs.microsoft.com/en-us/rest/api/storageservices/put-block-from-url</c> for Put Block From URL,
+        /// <c>https://docs.microsoft.com/en-us/rest/api/storageservices/append-block-from-url</c> for Append Block From URL,
+        /// <c>https://docs.microsoft.com/en-us/rest/api/storageservices/put-page-from-url</c> for Put Page From URL for details.
         /// </summary>
         ServiceSideSyncCopy
     }
