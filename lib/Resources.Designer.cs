bb1f54c46ed7ad415188ae4902428ef409c6b437
Emma Zhu <emmazhu@microsoft.com>
2019-12-11 16:32:41 +0800

    Upgrade to XSCL 11.1.1 Remove local file length limitation checking when destination is Azure File.

--- lib/Resources.Designer.cs
+++ lib/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Azure.Storage.DataMovement {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -169,7 +169,7 @@ namespace Microsoft.Azure.Storage.DataMovement {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to File size {0} is larger than cloud file maximum size {1} bytes..
+        ///   Looks up a localized string similar to The file size exceeds the maximum permissible limit..
         /// </summary>
         internal static string CloudFileSizeTooLargeException {
             get {
