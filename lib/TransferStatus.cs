4394f27be82ee553c6149b24e351f2be0e79569c
Amanda Nguyen <amnguye@microsoft.com>
2021-04-02 13:51:56 -0700

    Send service version with 4TB file share support and directory progress tracking for files

--- lib/TransferStatus.cs
+++ lib/TransferStatus.cs
@@ -27,7 +27,8 @@ namespace Microsoft.Azure.Storage.DataMovement
         /// <param name="numberOfFilesTransferred">Number of files that have been transferred.</param>
         /// <param name="numberOfFilesSkipped">Number of files that are skipped to be transferred.</param>
         /// <param name="numberOfFilesFailed">Number of files that are failed to be transferred.</param>
-        public TransferStatus(long bytesTransferred, long numberOfFilesTransferred, long numberOfFilesSkipped, long numberOfFilesFailed)
+        /// <param name="numberOfDirectoriesCreated">Number of directories that are created.</param>
+        public TransferStatus(long bytesTransferred, long numberOfFilesTransferred, long numberOfFilesSkipped, long numberOfFilesFailed, long numberOfDirectoriesCreated = 0)
         {
             if (bytesTransferred < 0)
             {
@@ -50,6 +51,7 @@ namespace Microsoft.Azure.Storage.DataMovement
             this.NumberOfFilesTransferred = numberOfFilesTransferred;
             this.NumberOfFilesSkipped = numberOfFilesSkipped;
             this.NumberOfFilesFailed = numberOfFilesFailed;
+            this.NumberOfDirectoriesCreated = numberOfDirectoriesCreated;
         }
 
         /// <summary>
@@ -83,6 +85,15 @@ namespace Microsoft.Azure.Storage.DataMovement
         /// Gets the number of files that are failed to be transferred.
         /// </summary>
         public long NumberOfFilesFailed
+        {
+            get;
+            internal set;
+        }
+
+        /// <summary>
+        /// Gets the number of directories created.
+        /// </summary>
+        public long NumberOfDirectoriesCreated
         {
             get;
             internal set;
