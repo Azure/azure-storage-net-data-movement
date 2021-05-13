b04581e3d238dd2791fa489dfd11597bdf85d86d
Amanda Nguyen <amnguye@microsoft.com>
2021-05-13 09:24:20 -0700

    Update changelog; update transfer manager to accept overload

--- lib/TransferStatus.cs
+++ lib/TransferStatus.cs
@@ -27,8 +27,40 @@ namespace Microsoft.Azure.Storage.DataMovement
         /// <param name="numberOfFilesTransferred">Number of files that have been transferred.</param>
         /// <param name="numberOfFilesSkipped">Number of files that are skipped to be transferred.</param>
         /// <param name="numberOfFilesFailed">Number of files that are failed to be transferred.</param>
+        public TransferStatus(long bytesTransferred, long numberOfFilesTransferred, long numberOfFilesSkipped, long numberOfFilesFailed)
+        {
+            if (bytesTransferred < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(bytesTransferred));
+            }
+            if (numberOfFilesTransferred < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(numberOfFilesTransferred));
+            }
+            if (numberOfFilesSkipped < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(numberOfFilesSkipped));
+            }
+            if (numberOfFilesFailed < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(numberOfFilesFailed));
+            }
+
+            this.BytesTransferred = bytesTransferred;
+            this.NumberOfFilesTransferred = numberOfFilesTransferred;
+            this.NumberOfFilesSkipped = numberOfFilesSkipped;
+            this.NumberOfFilesFailed = numberOfFilesFailed;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TransferStatus"/> class.
+        /// </summary>
+        /// <param name="bytesTransferred">Number of bytes that have been transferred.</param>
+        /// <param name="numberOfFilesTransferred">Number of files that have been transferred.</param>
+        /// <param name="numberOfFilesSkipped">Number of files that are skipped to be transferred.</param>
+        /// <param name="numberOfFilesFailed">Number of files that are failed to be transferred.</param>
         /// <param name="numberOfDirectoriesCreated">Number of directories that are created.</param>
-        public TransferStatus(long bytesTransferred, long numberOfFilesTransferred, long numberOfFilesSkipped, long numberOfFilesFailed, long numberOfDirectoriesCreated = 0)
+        public TransferStatus(long bytesTransferred, long numberOfFilesTransferred, long numberOfFilesSkipped, long numberOfFilesFailed, long numberOfDirectoriesCreated)
         {
             if (bytesTransferred < 0)
             {
