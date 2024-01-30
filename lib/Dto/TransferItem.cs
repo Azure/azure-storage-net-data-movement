76d4ffba11199dccd6343c840ba13500d487a530
Kacper Niwczyk <kacper.niwczyk@relativity.com>
2024-01-30 15:22:36 +0100

    Added better comments

--- lib/Dto/TransferItem.cs
+++ lib/Dto/TransferItem.cs
@@ -2,9 +2,16 @@
 
 namespace Microsoft.Azure.Storage.DataMovement.Dto
 {
-    // <summary> Represents one transfer item that is combined of source path and destination information.</summary>
+    /// <summary>
+    /// Represents one transfer item that is combined of source path and destination information.
+    /// </summary>
     public sealed class TransferItem
     {
+        /// <summary>
+        /// Default constructor for Transfer item.
+        /// </summary>
+        /// <param name="source">Source path.</param>
+        /// <param name="destination">Destination blob.</param>
         public TransferItem(string source, IListBlobItem destination)
         {
             Source = source;
