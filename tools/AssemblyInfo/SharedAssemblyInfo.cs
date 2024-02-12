adedf146e73de55c36c9ef446ecd34ebac108e27
Łukasz Krzykowski <lukasz.krzykowski@relativity.com>
2024-02-12 17:56:31 +0100

    chore: bump version to 4.6.0

--- tools/AssemblyInfo/SharedAssemblyInfo.cs
+++ tools/AssemblyInfo/SharedAssemblyInfo.cs
@@ -13,8 +13,8 @@ using System.Resources;
 using System.Runtime.InteropServices;
 
 #pragma warning disable CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision 
-[assembly: AssemblyVersion("4.5.1")]
-[assembly: AssemblyFileVersion("4.5.1")]
+[assembly: AssemblyVersion("4.6.0")]
+[assembly: AssemblyFileVersion("4.6.0")]
 
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Microsoft Azure Storage")]
